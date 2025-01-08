e914a7bd833a32c6129a06685e8f2f8541c28d1b
Zhang Zelun <33217105+BluebirdStory@users.noreply.github.com>
2025-01-08 15:07:07 +0800

    Fix pil (#2793)

--- paddlex/inference/models_new/semantic_segmentation/result.py
+++ paddlex/inference/models_new/semantic_segmentation/result.py
@@ -26,6 +26,8 @@ class SegResult(BaseCVResult):
         """apply"""
         seg_map = self["pred"]
         pc_map = self.get_pseudo_color_map(seg_map[0])
+        if pc_map.mode == "P":
+            pc_map = pc_map.convert("RGB")
         return pc_map
 
     def get_pseudo_color_map(self, pred):
