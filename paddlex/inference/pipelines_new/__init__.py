e7ce2ec75b685abda455891897f9defa8291e63a
zxcd <228587199@qq.com>
2025-01-13 11:38:32 +0800

    add whisper pipeline (#2789)

--- paddlex/inference/pipelines_new/__init__.py
+++ paddlex/inference/pipelines_new/__init__.py
@@ -26,6 +26,7 @@ from .image_classification import ImageClassificationPipeline
 from .object_detection import ObjectDetectionPipeline
 from .seal_recognition import SealRecognitionPipeline
 from .table_recognition import TableRecognitionPipeline
+from .multilingual_speech_recognition import MultilingualSpeechRecognitionPipeline
 from .formula_recognition import FormulaRecognitionPipeline
 from .video_classification import VideoClassificationPipeline
 from .anomaly_detection import AnomalyDetectionPipeline
